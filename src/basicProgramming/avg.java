beedfe840555000af4bedb750b281842d6e2336b
openai22 <madhavbaruma@gmail.com>
2025-07-26 11:21:35 +0530

    Add files via upload

---
+++ src/basicProgramming/avg.java
@@ -0,0 +1,10 @@
+package basicProgramming;
+
+public class avg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/basicProgramming/types1.java
@@ -0,0 +1,16 @@
+package basicProgramming;
+
+public class types1 {
+
+	public static void main(String[] args) {
+		int intVal = 100;
+        long longVal = intVal;        
+        float floatVal = longVal;    
+
+        System.out.println("Integer value: " + intVal);
+        System.out.println("Long value   : " + longVal);
+        System.out.println("Float value  : " + floatVal);
+
+	}
+
+}
