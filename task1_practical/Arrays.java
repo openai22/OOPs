8df9695ea0b85f2bf8d22a7a648a9b2bee39f03a
openai22 <madhavbaruma@gmail.com>
2025-07-26 10:11:58 +0530

    Create Arrays.java

---
+++ task1_practical/Arrays.java
@@ -0,0 +1,15 @@
+package task1_practical;
+
+public class Arrays {
+
+  public static void main(String[] args) {
+    
+    int[] arr = {1,20,29,36,4000};
+    
+    for(int i = 0;i<arr.length;i++)
+    {
+      System.out.println(arr[i]);
+    }
+  }
+
+}
